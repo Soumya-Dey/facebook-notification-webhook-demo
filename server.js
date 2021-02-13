8e26bf8fe91f560a5b6263f03962096d67989f05
Soumya Dey <soumyadey200@hotmail.com>
2021-02-13 19:24:44 +0530

    added dotenv

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 
-app.get('/', (req, res) => res.send('Hello from localhost:5000'));
+app.get('/', (req, res) => res.send(`Hello from localhost:${PORT}`));
 
 app.listen(PORT, () =>
-  console.log('server started at => http://localhost:5000/')
+  console.log(`server started at => http://localhost:${PORT}/`)
 );
