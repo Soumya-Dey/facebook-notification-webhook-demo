9d8fb9ced0d2970efba490a49b5d08ba47f38cbc
Soumya Dey <soumyadey200@hotmail.com>
2021-02-13 20:34:26 +0530

    updated POST webhook

--- server.js
+++ server.js
@@ -13,24 +13,24 @@ app.use(cors());
 
 app.get('/', (req, res) => res.send(`Hello from localhost:${PORT}`));
 
-// Creates the endpoint for our webhook
 app.post('/webhook', (req, res) => {
+  // Parse the request body from the POST
   let body = req.body;
 
-  // Checks this is an event from a page subscription
+  // Check the webhook event is from a Page subscription
   if (body.object === 'page') {
-    // Iterates over each entry - there may be multiple if batched
+    // Iterate over each entry - there may be multiple if batched
     body.entry.forEach(function (entry) {
-      // Gets the message. entry.messaging is an array, but
-      // will only ever contain one message, so we get index 0
+      // Get the webhook event. entry.messaging is an array, but
+      // will only ever contain one event, so we get index 0
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
     });
 
-    // Returns a '200 OK' response to all requests
+    // Return a '200 OK' response to all events
     res.status(200).send('EVENT_RECEIVED');
   } else {
-    // Returns a '404 Not Found' if event is not from a page subscription
+    // Return a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404);
   }
 });
